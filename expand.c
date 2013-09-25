7d462eba443f18d93bc586810b4eb1b94991b5ca
Bulwersator <bulwersator@gmail.com>
2013-09-25 14:14:30 +0200

    detect weird values of bicycle and cycleway tags

--- expand.c
+++ expand.c
@@ -4,16 +4,18 @@ gcc -E expand.c > test.txt
 to expand macros. Run regexp "^" to add tabs, than replace part of Biking.mrules
 this hack is hilarious ugly, but better than editing expanded form of this stuff
 */
-
+//TODO handle cycleway=opposite (droga dwukierunkowa dla rowerzystów, dla reszty jednokierunkowa)
 #define __cycleable (highway=cycleway OR cycleway=lane OR bicycle = yes OR bicycle = designated OR cycleway=opposite_lane)
 #define __typical_road @isOneOf(highway, trunk, trunk_link, primary, primary_link, secondary, tertiary, pedestrian, residential, living_street, unclassified)
 #define __no_surface_for_lane ((cycleway=lane OR segregated = yes) AND NOT cycleway:right = surface* AND NOT cycleway:left = surface*) //not used, as lane may be the same as main
 #define __paved_is_surface_for_lane ((cycleway=lane OR segregated = yes) AND (cycleway:right = "surface=paved" OR cycleway:left = "surface=paved"))
-#define __separate_cycleway (highway=cycleway AND NOT segregated = no AND NOT foot = yes AND NOT foot = designated)
+#define __separate_cycleway ((highway=cycleway AND NOT segregated = no AND NOT foot = yes AND NOT foot = designated) OR (cycleway = track))
 #define __segregated_cycleway ((highway=cycleway AND segregated = yes) OR (bicycle=designated AND segregated = yes) OR (cycleway = lane))
 #define __proper_surface (surface = asphalt OR smoothness = excellent OR cycleway:right = "surface=asphalt" OR cycleway:left = "surface=asphalt")
 #define __cycleway (__separate_cycleway OR __segregated_cycleway)
 
+weird cycleway value : cycleway AND NOT cycleway=lane AND NOT cycleway=opposite_lane AND NOT cycleway=no AND NOT cycleway=opposite AND NOT cycleway=track
+weird bicycle value : bicycle AND NOT bicycle=yes AND NOT bicycle=no AND NOT bicycle = designated AND NOT bicycle = dismount
 no_and_yes_bug : __cycleable AND bicycle=no
 crossing_as_way_rather_than_node_bug : highway = crossing
 no oneway for bicycle instead of opposite_lane : "oneway:bicycle" = "no" AND NOT cycleway=opposite_lane
