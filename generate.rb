1e99888bc577e5d950963c2e0c35c2c3c9e1bba2
Bulwersator <bulwersator@gmail.com>
2013-10-20 12:55:10 +0200

    drop requests for not needed (now) surface data

--- generate.rb
+++ generate.rb
@@ -54,7 +54,7 @@ if debug
 	puts "		no_and_yes_bug : #{__cycleable} AND bicycle=no"
 	puts "		crossing_as_way_rather_than_node_bug : highway = crossing"
 	puts "		no oneway for bicycle instead of opposite_lane : \"oneway:bicycle\" = \"no\" AND NOT cycleway=opposite_lane"
-	puts "		no_surface_info : (#{__cycleway} OR #{__lame_cycleway} OR #{__contraflow}) AND (#{__no_surface_info_for_main_part} AND #{__no_surface_info_for_lane})"
+	puts "		no_surface_info : (#{__cycleway} OR #{__lame_cycleway}) AND (#{__no_surface_info_for_main_part} AND #{__no_surface_info_for_lane})"
 	puts "		no maxspeed info : (@isOneOf(highway, trunk, trunk_link, primary, primary_link, secondary, tertiary, unclassified) AND NOT maxspeed)"
 	puts "		plus 50 maxspeed : (maxspeed > 50)"
 	puts "		missing segregate : ((bicycle=designated OR highway=cycleway) AND (foot=yes OR foot=designated OR bicycle=foot OR highway=footway) AND NOT segregated)"
