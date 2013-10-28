7d0be3dc1507ab6fb04318ce8bf223b2619abaf4
Bulwersator <bulwersator@gmail.com>
2013-10-28 20:20:02 +0100

    save full file as interpreter.osm

--- download.rb
+++ download.rb
@@ -12,17 +12,19 @@ if downloading
 	text = RestClient.get(URI.escape("http://overpass-api.de/api/interpreter?data=(node(50,19.78,50.11,20.09);<;);out meta;"), :user_agent => "bulwersator@gmail.com").to_str
 	puts "downloading: end"
 
-	filename = "interpreter"
-	if File.exists?(filename)
-		puts "deleting old file: start"
-		File.delete(filename)
-		puts "deleting old file: end"
-	end
-	puts "saving new file: start"
-	f = File.new(filename, "w")
-	f.write text
-	f.close
-	puts "saving new file: end"
+	filenames = ["interpreter", "interpreter.osm"]
+	filenames.each {|filename|
+		if File.exists?(filename)
+			puts "deleting old file: start"
+			File.delete(filename)
+			puts "deleting old file: end"
+		end
+		puts "saving new file: start"
+		f = File.new(filename, "w")
+		f.write text
+		f.close
+		puts "saving new file: end"
+	}
 end
 
 puts "filtering, I phase: start"
