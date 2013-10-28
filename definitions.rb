100f3d1f55251650f736d2592224e41ea029e413
Bulwersator <bulwersator@gmail.com>
2013-10-28 08:58:23 +0100

    show impassable lines (railways, motorways)

--- definitions.rb
+++ definitions.rb
@@ -6,6 +6,9 @@ end
 
 def get_bottom_before_bicycle_styles 
 return "
+	lines
+		impassable: (railway=rail OR highway=motorway OR highway = motorway_link) AND NOT tunnel AND NOT bridge
+
 properties
 	map-background-color	: white
 	text-halo-width : 30%
@@ -20,11 +23,19 @@ return "
 		define
 			min-zoom : 1
 			line-width : 1
-			line-color : gray
+			line-color : #707070
 		draw: line
 	target: cycleable road
 		define
 			min-zoom : 16
 		draw: text
+
+	target: impassable
+		define
+			min-zoom : 1
+			line-width : 2
+			line-color : #404040
+			line-style : solid
+		draw: line
 "
 end
