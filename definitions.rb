0ca8a4a8c3217f25622834641e981358beb7f504
Bulwersator <bulwersator@gmail.com>
2013-12-01 19:21:06 +0100

    unify coding style

--- definitions.rb
+++ definitions.rb
@@ -15,9 +15,9 @@ return "
 	lines
 		water-line: (waterway=stream OR waterway=river OR waterway=canal) AND NOT tunnel
 properties
-	map-background-color	: white
-	text-halo-width : 30%
-	text-halo-opacity : 0.8
+	map-background-color: white
+	text-halo-width: 30%
+	text-halo-opacity: 0.8
 
 rules
 "
@@ -26,47 +26,47 @@ def get_bottom_after_bicycle_styles
 return "
 	target: cycleable road
 		define
-			min-zoom : 1
-			line-width : 1
-			line-color : #707070
+			min-zoom: 1
+			line-width: 1
+			line-color: #707070
 		draw: line
 	target: cycleable road
 		define
-			min-zoom : 16
+			min-zoom: 16
 		draw: text
 
 	target: motorway
 		define
-			min-zoom : 1
-			line-width : 2
-			line-color : #404040
-			line-style : solid
+			min-zoom: 1
+			line-width: 2
+			line-color: #404040
+			line-style: solid
 		draw: line
 		
 	target: blocked area
 		define
 			fill-color: #bfbfbf
-			line-width : 0
-			line-color : #bfbfbf
+			line-width: 0
+			line-color: #bfbfbf
 		draw: fill
 
 	target: water
 		define
 			fill-color: #b0b0ef
-			line-width : 0
-			line-color : #b0b0ef
-		draw : fill
+			line-width: 0
+			line-color: #b0b0ef
+		draw: fill
 	target: water-line
 		define
-			line-width : 2
-			line-color : #b0b0ef
-		draw : line
+			line-width: 2
+			line-color: #b0b0ef
+		draw: line
 
 	target: trees
 		define
 			fill-color: #a5ef9b
-			line-width : 0
-			line-color : #a5ef9b
-		draw : fill
+			line-width: 0
+			line-color: #a5ef9b
+		draw: fill
 "
 end
