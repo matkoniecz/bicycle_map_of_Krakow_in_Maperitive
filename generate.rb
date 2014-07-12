44ce95c836c1557a484f4a0e33c201982524b9d4
Mateusz Konieczny <matkoniecz@gmail.com>
2014-07-12 10:49:42 +0200

    stop asking for maxspeed on trunk and trunk_link

--- generate.rb
+++ generate.rb
@@ -75,7 +75,7 @@ if debug
 	puts "		weird surface value: #{cycleable} AND (#{weird_main_surface} OR #{weird_cycleway_surface})"
 	puts "		no_and_yes_bug: (#{cycleable} AND bicycle=no) OR (highway=cycleway AND bicycle AND NOT bicycle = designated AND NOT bicycle = dismount)"
 	puts "		no_surface_info: (#{cycleway} OR #{not_segregated_cycleway}) AND (#{no_surface_info_for_main_part} AND #{no_surface_info_for_lane})"
-	puts "		no maxspeed info: (@isOneOf(highway, trunk, trunk_link, primary, primary_link, secondary, secondary_link) AND NOT maxspeed)"
+	puts "		no maxspeed info: (@isOneOf(highway, primary, primary_link, secondary, secondary_link) AND NOT maxspeed)"
 	puts "		plus 50 maxspeed: (maxspeed > 50 AND NOT #{motorway})"
 	puts "		missing segregated: ((bicycle=designated OR highway=cycleway) AND (foot=yes OR foot=designated OR bicycle=foot OR highway=footway) AND NOT segregated AND NOT area:highway)"
 	puts "		weird segregated: #{weird("segregated", ["yes", "no"])}"
