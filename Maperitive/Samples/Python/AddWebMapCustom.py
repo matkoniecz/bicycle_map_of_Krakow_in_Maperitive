2aba045799558e765507de2423c027bdf4e35c20
Mateusz Konieczny <matkoniecz@gmail.com>
2015-09-13 22:56:06 +0200

    Update Maperitive

--- Maperitive/Samples/Python/AddWebMapCustom.py
+++ Maperitive/Samples/Python/AddWebMapCustom.py
@@ -9,4 +9,4 @@ Map.clear()
 
 # the first argument is the name of your custom map
 # the second is the base URL of the Web map tiles (or multiple URLs if you have them)
-Map.add_web_map_custom("4umaps.eu", ["http://176.28.41.237" ])
+Map.add_web_map_custom("4umaps.eu", ["http://4umaps.eu" ])
